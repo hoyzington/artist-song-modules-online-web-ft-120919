5e300b941e1fd677dd5d710ae3615477dd8d34a7
Scott Hoisington <hoyzington@gmail.com>
2020-01-07 05:29:44 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -7,19 +7,19 @@ class Artist
   attr_accessor :name
   attr_reader :songs
 
-  @@artists = []
+  @@all = []
 
   def initialize
-    @@artists << self
+    @@all << self
     @songs = []
   end
 
   def self.find_by_name(name)
-    @@artists.detect{|a| a.name == name}
+    @@all.detect{|a| a.name == name}
   end
 
   def self.all
-    @@artists
+    @@all
   end
 
 #      def self.reset_all
